589629156036ae9c30bb065f69d49bbf5dba685b
Rakesh Verma <rakeshbond009@gmail.com>
2026-03-25 23:49:24 +0530

    Auto Update v1.0.2764 via Admin Panel: 2026-03-25 23:49:24

--- includes/init.php
+++ includes/init.php
@@ -1,5 +1,5 @@
 <?php
-if (!defined('APP_VERSION')) define('APP_VERSION', '1.0.2761');
+if (!defined('APP_VERSION')) define('APP_VERSION', '1.0.2764');
 /**
  * GATEPILOT - COMPLETE VERSION
  * Features: Inward/Outward, QR Scanning, Vehicle Fetch, Dashboard, Reports, Admin Panel
