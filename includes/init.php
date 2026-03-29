16f43eccd5d67815f189241ad34688a755934b0c
Rakesh Verma <rakeshbond009@gmail.com>
2026-03-29 15:16:31 +0530

    Auto Update v26.03.29.1516: 2026-03-29 15:16:31

--- includes/init.php
+++ includes/init.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('APP_VERSION'))
-    define('APP_VERSION', '26.03.29.1514');
+    define('APP_VERSION', '26.03.29.1516');
 /**
  * GATEPILOT - COMPLETE VERSION
  * Features: Inward/Outward, QR Scanning, Vehicle Fetch, Dashboard, Reports, Admin Panel
